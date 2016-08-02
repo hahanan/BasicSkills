21db7f25f78ec3df47bcb0fddd7a04047f0fce4e
hahanan <15801207364@163.com>
2016-08-02 17:15:49 +0800

    添加开机启动页以及设置AppIcon的项目

---
+++ AppiconAndLaunchImage/LaunchScreenView/LaunchScreenView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LaunchScreenView
+//
+//  Created by 王康棋 on 16/8/2.
+//  Copyright © 2016年 Alara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
